4500d852c1464a7d4c068bda4031aaeb0dc18c21
Kristoffer Hilmersson <dat11khi@student.lu.se>
2015-04-16 16:38:53 +0200

    so very hiTechGyro

--- LegoSegway/src/regul/Segway.java
+++ LegoSegway/src/regul/Segway.java
@@ -3,12 +3,12 @@ package regul;
 public class Segway {
 	private RegulatedMotor leftMotor;
     private RegulatedMotor rightMotor;
-    private TiltSensor tiltSensor;
+    private HiTechnicGyro gyro;
 	
-	public Segway(RegulatedMotor leftMotor, RegulatedMotor rightMotor, TiltSensor tiltSensor) {
+	public Segway(RegulatedMotor leftMotor, RegulatedMotor rightMotor, HiTechnicGyro gyro) {
         this.leftMotor = left;
         this.rightMotor = right;
-        this.tiltSensor = tiltSensor;
+        this.gyro = gyro;
 	}
 
 }
