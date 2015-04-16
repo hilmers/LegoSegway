22078a5b96c1647ba5ffcdc297f8ba73a024235d
Erik Stenlund <erikstenlund0810@gmail.com>
2015-04-16 15:54:52 +0200

    client++

--- LegoSegway/src/client/OperatorPlotGUI.java
+++ LegoSegway/src/client/OperatorPlotGUI.java
@@ -6,37 +6,40 @@ import javax.swing.*;
 
 public class OperatorPlotGUI{
 	private int controlSignal, currentAngle, referenceValue;
-	JLabel jl;
+	JTextArea jt;
 	public OperatorPlotGUI() {
 		JFrame frame = new JFrame();
-		jl = new JLabel();
-		init(frame, jl);	
+		jt = new JTextArea();
+		init(frame, jt);	
 		setSignals(0, 0, 0);
 		
 		
 		
 	}
 	
-	private void init(JFrame frame, JLabel jl) {
+	private void init(JFrame frame, JTextArea jt) {
 		frame.setTitle("Segway Plot");
 		frame.setSize(300, 300);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		jt.setEditable(false);
 		frame.setLayout(new FlowLayout());
-		 frame.add(jl);
+		
+		frame.add(jt);
 		 
 		
 		 frame.setVisible(true);
 	}
 	
 	public void plot() {
-		String prevText = "";
-		if (jl.getText().length() > 0)
-			prevText = jl.getText().split("<html>")[1].split("</html>")[0];
+//		String prevText = "";
+//		if (jt.getText().length() > 0)
+//			prevText = jt.getText().split("<html>")[1].split("</html>")[0];
 		
-		
-		jl.setText("<html>"+ prevText+ "<br>ControlSignal: " + controlSignal + ", Angle: " + currentAngle + ", Ref: "+ referenceValue + "</html>");
+		jt.append("\nControlSignal: " + controlSignal + ", Angle: " + currentAngle + ", Ref: "+ referenceValue);
+
+		//jt.setText("<html>"+ prevText+ "<br>ControlSignal: " + controlSignal + ", Angle: " + currentAngle + ", Ref: "+ referenceValue + "</html>");
 		//Ska plotta ut i graf men först bara text
 	}
 	
