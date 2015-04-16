9f444e0c58d5d31e7643edb9fe3a7e5b1196a9ee
alex011235 <st.alexander85@gmail.com>
2015-04-16 14:18:55 +0200

    Test-gyro-class

--- LegoSegway/src/tests/TestGyro.java
+++ LegoSegway/src/tests/TestGyro.java
@@ -1,5 +1,34 @@
 package tests;
 
-public class TestGyro {
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.HiTechnicGyro;
+
+public class TestGyro implements Runnable {
+
+	@Override
+	public void run() {
+		
+		@SuppressWarnings("resource")
+		HiTechnicGyro gyro = new HiTechnicGyro(SensorPort.S2);
+		while(true){
+			try {
+				Thread.sleep(1000);
+				int curr = gyro.getCurrentMode();	
+				System.out.println(curr);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new TestGyro());
+		thread.start();
+	}
 
 }
