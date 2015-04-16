dd82f80e90ae532f5e80fac4b3fc9ba292fc96ac
Kristoffer Hilmersson <dat11khi@student.lu.se>
2015-04-16 16:33:14 +0200

    lite inparameter

--- LegoSegway/src/regul/Segway.java
+++ LegoSegway/src/regul/Segway.java
@@ -1,10 +1,14 @@
 package regul;
 
 public class Segway {
+	private RegulatedMotor leftMotor;
+    private RegulatedMotor rightMotor;
+    private TiltSensor tiltSensor;
 	
-	
-	public Segway() {
-		
+	public Segway(RegulatedMotor leftMotor, RegulatedMotor rightMotor, TiltSensor tiltSensor) {
+        this.leftMotor = left;
+        this.rightMotor = right;
+        this.tiltSensor = tiltSensor;
 	}
 
 }
