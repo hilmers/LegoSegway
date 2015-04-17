1b25caea56b7d7f7936a39c9770c610bbf2d7b1b
alex011235 <st.alexander85@gmail.com>
2015-04-17 09:28:15 +0200

    lite smått och gott i simplecontroller

--- LegoSegway/src/hardware/SimpleController.java
+++ LegoSegway/src/hardware/SimpleController.java
@@ -1,33 +1,51 @@
 package hardware;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
 public class SimpleController implements Runnable {
 	private GyroSensor gyro;
 	private SegwayMotor left, right;
-	
-	
-	public SimpleController(GyroSensor gyro, SegwayMotor left, SegwayMotor right){
-		
+	private long samplInterv;
+
+	public SimpleController(GyroSensor gyro, SegwayMotor left,
+			SegwayMotor right, long sampInterv) {
+		this.gyro = gyro;
+		this.left = left;
+		this.right = right;
+		this.samplInterv = sampInterv;
 	}
-	
-	
-	
+
 	public void run() {
-		
-		
+		float sensorValue;
+		while (!Thread.interrupted()) {
+
+			
+			
+			
+			
+			Delay.msDelay(samplInterv);
+			
+			
+			
+
+		}
+
 	}
-	
-	
-	
-	
-	
-	
 
 	public static void main(String[] args) {
-		
+		RegulatedMotor motor_left = new EV3LargeRegulatedMotor(MotorPort.C);
+		RegulatedMotor motor_right = new EV3LargeRegulatedMotor(MotorPort.B);
+		SegwayMotor left = new SegwayMotor(motor_left);
+		SegwayMotor right = new SegwayMotor(motor_right);
 		GyroSensor gyro = new GyroSensor(0.6f, 100);
-		// TODO Auto-generated method stub
+		SimpleController controller = new SimpleController(gyro, left, right,
+				100);
+		Thread thread = new Thread(controller);
+		thread.start();
 
 	}
-
 
 }
