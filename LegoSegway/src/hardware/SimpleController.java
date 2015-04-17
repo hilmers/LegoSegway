0afe96e6f9aec62f4583f4492c62833261958045
alex011235 <st.alexander85@gmail.com>
2015-04-17 09:12:20 +0200

    Lagt till simplecontroller

---
+++ LegoSegway/src/hardware/SimpleController.java
@@ -0,0 +1,33 @@
+package hardware;
+
+public class SimpleController implements Runnable {
+	private GyroSensor gyro;
+	private SegwayMotor left, right;
+	
+	
+	public SimpleController(GyroSensor gyro, SegwayMotor left, SegwayMotor right){
+		
+	}
+	
+	
+	
+	public void run() {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		GyroSensor gyro = new GyroSensor(0.6f, 100);
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
