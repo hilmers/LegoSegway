1784a0c81ef8cc1de9239f61c44b0c6824946943
Erik Stenlund <erikstenlund0810@gmail.com>
2015-05-14 11:42:46 +0200

    main upd

--- LegoSegway/src/client/Main.java
+++ LegoSegway/src/client/Main.java
@@ -7,26 +7,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
-		OperatorPlotGUI opGUI = new OperatorPlotGUI();
-		  Opcom opcom = new Opcom();
-		  Monitor mon = new Monitor();
-		  opcom.initializeGUI();
-		  Reader reader = new Reader(opcom, mon);
-		  opcom.start();
-		  reader.start();
-		  CommunicationThread t = new CommunicationThread(mon);
-		
-		//ReferenceGUI refGUI = new ReferenceGUI(mon);
-		PlotThread pt = new PlotThread(mon, opGUI);
-		Thread t1 = new Thread(pt);
-		Thread t2 = new Thread(t);
-		t1.start();
-		t2.start();
-		
-		
-
+		Opcom opcom = new Opcom();
+		Monitor mon = new Monitor();
+		opcom.initializeGUI();
+		Reader reader = new Reader(opcom, mon);
+		opcom.start();
+		reader.start();
+		CommunicationThread t = new CommunicationThread(mon);
+		Thread ct = new Thread(t);
+		ct.start();
+		// t2.start();
 
 	}
-	
 
 }
