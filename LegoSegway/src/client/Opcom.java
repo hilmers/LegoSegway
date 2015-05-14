4f566c60e3ae94890b83b33e8ef8dc2a933165bf
Erik Stenlund <erikstenlund0810@gmail.com>
2015-05-14 11:42:15 +0200

    Split opcom

--- LegoSegway/src/client/Opcom.java
+++ LegoSegway/src/client/Opcom.java
@@ -11,86 +11,9 @@ import java.util.*;
 import se.lth.control.realtime.*;
 
 
-class PlotData implements Cloneable { 
-    double ref, y; 
-    double x; // holds the current time 
-    
-    public Object clone() { 
-        try { 
-	    return super.clone(); 
-        } catch (Exception e) {return null;} 
-    } 
-} 
 
 
-class Reader extends Thread {
 
-    private Opcom opcom;
-    private Monitor mon;
-
-    private boolean doIt = true;
-
-	 double velChan, posChan, ctrlChan;
-
-    /** Constructor. Sets initial values of the controller parameters and initial mode. */
-    public Reader(Opcom opcom, Monitor mon) {
-    	this.mon =mon;
-		 this.opcom = opcom;
-
-    }
-
-    /** Run method. Sends data periodically to Opcom. */
-    public void run() {
-		  final long h = 25; // period (ms)
-		  long duration;
-		  long t = System.currentTimeMillis();
-		  DoublePoint dp;
-		  PlotData pd;
-		  double vel = 0, pos = 0, ctrl = 0;
-		  double realTime = 0;
-
-		 
-		
-
-		  setPriority(7);
-
-		  while (doIt) {
-				vel = mon.getCurrentAngle();
-				pos = mon.getReferenceValue();
-				ctrl = mon.getControlSignal();
-		
-				pd = new PlotData();
-				pd.y = vel;
-				pd.ref = pos;
-				pd.x = realTime;
-				opcom.putMeasurementDataPoint(pd);
-	    
-				dp = new DoublePoint(realTime,ctrl);
-				opcom.putControlDataPoint(dp);
-
-				realTime += ((double) h)/1000.0;
-
-				t += h;
-				duration = (int) (t - System.currentTimeMillis());
-				if (duration > 0) {
-					 try {
-						  sleep(duration);
-					 } catch (Exception e) {}
-				}
-		  }
-    }
-
-    /** Stops the thread. */
-    private void stopThread() {
-		  doIt = false;
-    }
-
-    /** Called by Opcom when the Stop button is pressed. */
-    public synchronized void shutDown() {
-		  stopThread();
-    } 
-
-}
 
 
 /** Class that creates and maintains a GUI for the Ball and Beam process. 
